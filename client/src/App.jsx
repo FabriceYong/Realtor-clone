2006a9ab04da0618665a16dab14bd1d40eff244f
Fabrice Yong <fabricenjua@gmail.com>
2023-12-15 19:07:03 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import ForgotPassword from './pages/ForgotPassword'
+import Offers from './pages/Offers'
 
 function App() {
 
   return (
     <>
-      <h1>Realtor clone</h1>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route  exact path='/profile' element={<Profile />} />
+          <Route exact path='/signin' element={<SignIn />} />
+          <Route exact path='/signup' element={<SignUp />} />
+          <Route exact path='/forgot-password' element={<ForgotPassword />} />
+          <Route exact path='/offers' element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   )
 }
